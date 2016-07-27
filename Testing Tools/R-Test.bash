c47e42b8be3491cba4b79f85ce55e96c945c15ca
stephenfedele <stephen.f@corephp.com>
2016-07-27 11:15:42 -0400

    Update for R-Test.bash
    Updated the test so that it could chunk data into multiple files for download.

--- Testing Tools/R-Test.bash
+++ Testing Tools/R-Test.bash
@@ -1,46 +1,50 @@
 #!/bin/bash
 
 SAVEFOLDER=$HOME/Downloads
+BROWSER="chromium-browser"
+
+RPlot(){
+	R --no-save << EOF &>/dev/null &
+	x11();
+	attach(read.csv("$2"));
+	barplot(table($1),breaks=(max($1)-min($1)+1));
+	Sys.sleep(999999999999);
+EOF
+}
 
 if [ -e $SAVEFOLDER/raw.txt ]
 then
 	rm -f $SAVEFOLDER/raw.txt
 fi
-
 if [ -e ./data.txt ]
 then
 	rm -f ./data.txt
 fi
-
-if [ -e ./stats.txt ]
+if [ -e ./stats/ ]
 then
-	rm -f ./stats.txt
+	rm -Rf ./stats/
 fi
 
-setsid chromium-browser ./libTest.html &>/dev/null &
+mkdir ./stats/
+$BROWSER &>/dev/null &
 
-sleep 5
+for INDEX in $(seq 0 1 7)
+do
+	$BROWSER ./libTest.html &>/dev/null &
+	sleep 2.5
+	cat $SAVEFOLDER/raw.txt >>./data.txt
+	rm -f $SAVEFOLDER/raw.txt
+done
 
-mv $SAVEFOLDER/raw.txt ./data.txt
-
-unset -v SAVEFOLDER
+killall $BROWSER
+unset -v BROWSER INDEX SAVEFOLDER
 
 #Use this to find the positions of the embedded strings
-echo "indeces" >./stats.txt; sed -e "s:{{}:{:g" -e "s:^\(.*\)\ .*\$:\1:g" ./data.txt | awk '{print length}' >>./stats.txt
-
-setsid R --no-save << EOF &>/dev/null
-x11();
-attach(read.csv("./stats.txt"));
-hist(indeces,breaks=(max(indeces)-min(indeces)+1));
-Sys.sleep(999999999999);
-EOF
+echo "indeces" >./stats/indeces.txt; sed -e "s:{{}:{:g" -e "s:^\(.*\)\ .*\$:\1:g" ./data.txt | awk '{print length}' >>./stats/indeces.txt
+RPlot "indeces" ./stats/indeces.txt &
 
 #Use this to find the length of the strings
-#echo "lengths" >./stats.txt; sed -e "s:{{}:{:g" ./data.txt | awk '{print length}' >>./stats.txt
-#
-#setsid R --no-save << EOF &>/dev/null
-#x11();
-#attach(read.csv("./stats.txt"));
-#hist(lengths,breaks=(max(lengths)-min(lengths)+1));
-#Sys.sleep(999999999999);
-#EOF
+echo "lengths" >./stats/lengths.txt; sed -e "s:{{}:{:g" ./data.txt | awk '{print length}' >>./stats/lengths.txt
+RPlot "lengths" ./stats/lengths.txt &
+
+wait
